d89c50a7ac9a18ecac4f31fa404aebe04c121e5c
Rajeshwari Tamshetti <rajeshwaritamshetti2@gmail.com>
2026-07-01 18:42:40 +0530

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import dill
 from sklearn.metrics import r2_score
 
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -17,16 +19,24 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,X_test,y_train,y_test,models):
+def evaluate_model(X_train,X_test,y_train,y_test,models,params):
     try:
         report={}
         for i in range(len(list(models))):
-            model=list(models.values())[i]
+            model_name = list(models.keys())[i]
+            model = models[model_name]
+            para = params[model_name]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            y_train_pred=model.predict(X_train)
-            y_test_pred=model.predict(X_test)
+            
+
+            y_train_pred=model.predict(X_train)  #To get the predicted values for training data,for learning the model
+            y_test_pred=model.predict(X_test)  #To get the predicted values for testing data
 
             train_model_score=r2_score(y_train,y_train_pred)
             test_model_score=r2_score(y_test,y_test_pred)
